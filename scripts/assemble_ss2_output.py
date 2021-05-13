9702bab4097d6d50bef42f35922d3efaed250c5b
Alex Sickler <sicklera@chop.edu>
2021-05-13 16:06:41 -0400

    :pencil: fix tab spacing in assemble_ss2_output

--- scripts/assemble_ss2_output.py
+++ scripts/assemble_ss2_output.py
@@ -6,39 +6,39 @@ import argparse
 import loompy
 
 def parse_args(args):
- '''Get arguments.'''
- #required args
- parser = argparse.ArgumentParser()
- required_args = parser.add_argument_group("required arguments")
- required_args.add_argument(
-  "--files",
-  help="Comma separated list of paths to the RSEM output looms",
-  required=True
-  )
- required_args.add_argument(
-  "--base",
-  help="Output basename",
-  required=True
-  )
+    '''Get arguments.'''
+    #required args
+    parser = argparse.ArgumentParser()
+    required_args = parser.add_argument_group("required arguments")
+    required_args.add_argument(
+        "--files",
+        help="Comma separated list of paths to the RSEM output looms",
+        required=True
+        )
+    required_args.add_argument(
+        "--base",
+        help="Output basename",
+        required=True
+        )
 
- #parse and return arguments
- args = parser.parse_args()
- files = args.files
- base = args.base
+    #parse and return arguments
+    args = parser.parse_args()
+    files = args.files
+    base = args.base
 
- return files, base
+    return files, base
 
 def main(args):
- '''Main.'''
- #parse arguments
- files, base_name = parse_args(args)
- merged_loom = base_name + ".loom"
+    '''Main.'''
+    #parse arguments
+    files, base_name = parse_args(args)
+    merged_loom = base_name + ".loom"
 
- #make matrix loom
- loom_files = files.split(',')
- print(loom_files)
- loompy.combine(loom_files, output_file=merged_loom)
+    #make matrix loom
+    loom_files = files.split(',')
+    print(loom_files)
+    loompy.combine(loom_files, output_file=merged_loom)
 
 if __name__ == "__main__":
- # execute only if run as a script
- main(sys.argv)
+    # execute only if run as a script
+    main(sys.argv)
