35b00fc921709636ec9175784052f3636f142049
antoniachroni <chronia@chop.edu>
2023-10-27 11:26:41 -0400

    :notepad: convert to tsv, add doublet boolean col, make doublet output complete

--- scripts/seurat_filter.R
+++ scripts/seurat_filter.R
@@ -19,9 +19,9 @@ option_list <- list(
     help = "RDS object produced by SoupX"
   ),
   make_option(
-    opt_str = "--scrublet_csv",
+    opt_str = "--scdblfinder_tsv",
     type = "character",
-    help = "CSV file with doublet information produced by Scrublet"
+    help = "TSV file with doublet information produced by scDblFinder"
   ),
   make_option(
     opt_str = "--output_name",
@@ -33,22 +33,22 @@ option_list <- list(
 
 opts <- parse_args(OptionParser(option_list = option_list), print_help_and_exit = TRUE)
 if (is.null(opts$seurat_qc_rds) | is.null(opts$seurat_qc_rds) | is.null(opts$soupx_rds)) {
-    write("\n\nseurat_qc_rds, soupx_rds, and scrublet_csv must be provided! One or more are missing!", stderr())
+    write("\n\nseurat_qc_rds, soupx_rds, and scdblfinder_tsv must be provided! One or more are missing!", stderr())
     quit(status=1)
 }
 output_name <- opts$output_name
 seurat_qc_rds <- opts$seurat_qc_rds
 soupx_rds <- opts$soupx_rds
-scrublet_csv <- opts$scrublet_csv
+scdblfinder_tsv <- opts$scdblfinder_tsv
 
 write("\n\nLoading Files...", stderr())
 seurat_qc_obj <- readRDS(seurat_qc_rds)
 soupx_obj <- readRDS(soupx_rds)
-scrublet_df <- read.csv(scrublet_csv, sep = ",", header = FALSE, row.names=1)
+scDblFinder_df <- read.csv(scdblfinder_tsv, sep = "\t", header = TRUE, row.names=1)
 write("Done loading files!", stderr())
 
 write("Filtering Seurat Cells...", stderr())
-write("...Begining SoupX Filtration...", stderr())
+write("...Beginning SoupX Filtration...", stderr())
 # Report mRNA
 filter_cells <- colnames(seurat_qc_obj)[which(!(colnames(seurat_qc_obj) %in% colnames(soupx_obj)))] # Get Seurat cells not found in SoupX
 if (length(filter_cells) == 0) {
@@ -70,21 +70,19 @@ DefaultAssay(seurat_qc_obj) <- "RNA_filter"  # make new filter the default for f
 write("...RNA_filter assay added...", stderr())
 
 # Remove Scrublet-Identified Doublets from Seurat
-write("...Beginning Scrublet Filtration...", stderr())
-colnames(scrublet_df) <- c("doublet_score","is_doublet") # set scrublet colnames
-seurat_qc_obj <- AddMetaData(seurat_qc_obj, scrublet_df)
-
+write("...Beginning scDblFinder Filtration...", stderr())
+seurat_qc_obj <- AddMetaData(seurat_qc_obj, scDblFinder_df)
 # Report Doublets
-doublet_cells <- colnames(seurat_qc_obj)[which(seurat_qc_obj@meta.data$is_doublet == "True")]
+doublet_cells <- colnames(seurat_qc_obj)[which(seurat_qc_obj@meta.data$is_doublet == TRUE)]
 if (length(doublet_cells) == 0) {
     write("......No Seurat cells identified as doublets. No cells removed...", stderr())
 } else {
-    write("......Cells found in Seurat were identified by Scrublet as doublets! Removing the following cells from Seurat:", stderr())
+    write("......Cells found in Seurat were identified by scDblFinder as doublets! Removing the following cells from Seurat:", stderr())
     cat(doublet_cells, sep = "\n", file = stderr())
 }
 
-seurat_filtered_obj <- subset(seurat_qc_obj, cells = WhichCells(seurat_qc_obj, expression = is_doublet=="False"))
-write("...Scrublet Filtration Complete...", stderr())
+seurat_filtered_obj <- subset(seurat_qc_obj, cells = WhichCells(seurat_qc_obj, expression = is_doublet==FALSE))
+write("...scDblFinder Filtration Complete...", stderr())
 write("Seurat Filtering Complete!", stderr())
 
 write("Saving Filtered Seurat Object...", stderr())
